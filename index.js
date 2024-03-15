114c79dea7dcbdc905f825021d68e660e77dc5d6
Jack Knapp <145280359+Jack-Knapp@users.noreply.github.com>
2024-03-14 22:32:13 -0700

    Update index.js
    Add static dist

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ morgan.token('data', function getData(request){
   return ""
 })
 
-const app =express()
+const app = express()
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
